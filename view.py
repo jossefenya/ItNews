65225319ce922eb4abbed3182ac0cc64400454a2
jossefeyna <sergeishekotihin@gmail.com>
2018-02-23 19:28:32 +0300

    Added pages 1-6

--- view.py
+++ view.py
@@ -5,3 +5,33 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+
+@app.route('/page3')
+def page3():
+    return render_template('page3.html')
+
+
+@app.route('/page4')
+def page4():
+    return render_template('page4.html')
+
+
+@app.route('/page5')
+def page5():
+    return render_template('page5.html')
+
+
+@app.route('/page6')
+def page6():
+    return render_template('page6.html')
