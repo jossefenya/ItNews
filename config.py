fcfbb5e159037ec91dba362f9936fe88c2583c8b
jossefeyna <sergeishekotihin@gmail.com>
2018-03-09 11:19:16 +0300

    Database created

---
+++ config.py
@@ -0,0 +1,3 @@
+class Configuration():
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:google22A@localhost/blog'
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from app import app
 import view
+from app import db
+from posts.blueprint import posts
+
+app.register_blueprint(posts, url_prefix='/news')
 
 if __name__ == '__main__':
     app.run(debug=True)
